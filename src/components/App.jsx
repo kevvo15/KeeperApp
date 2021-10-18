2d1bcf7983d5a18d4d4887ab5c99050a28dbfcb9
Kevin Sandoval <kevinsandoval.co@gmail.com>
2021-10-17 20:01:02 -0600

    Turned CreateNote into an anonymous function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,19 @@ import Note from './Note';
 import Footer from './Footer';
 import notes from '../notes';
 
-function createNote(note) {
-    return (
-        <Note
-            key={note.key}
-            title={note.title}
-            content={note.content}
-        />
-    );
-}
-
 function App() {
     return(
         <div>
             <Header />
-            {notes.map(createNote)}
+            
+            {notes.map((note) => 
+                <Note 
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            )}
+
             <Footer />
         </div>
     );
