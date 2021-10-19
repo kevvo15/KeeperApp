e799e3f93437dddf159b9d81d6e913008692cb1d
Kevin Sandoval <kevinsandoval.co@gmail.com>
2021-10-18 22:19:47 -0600

    Preparing Keeper App for final updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import Header from './Header';
-import Note from './Note';
-import Footer from './Footer';
-import notes from '../notes';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return(
-        <div>
-            <Header />
-            
-            {notes.map((note) => 
-                <Note 
-                    key={note.key}
-                    title={note.title}
-                    content={note.content}
-                />
-            )}
-
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
